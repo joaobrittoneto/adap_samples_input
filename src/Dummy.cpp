33f4678cfaa010d4685c921d62193c8e124a8621
Joao Neto <joao.neto@fieb.org.br>
2014-10-29 13:25:28 +0100

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,11 @@
+#include "Dummy.hpp"
+
+namespace adap_samples_input
+{
+
+	void DummyClass::welcome()
+	{
+		std::cout << "You successfully compiled and executed DummyProject. Welcome!" << std::endl;
+	}
+
+}
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <adap_samples_input/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+	adap_samples_input::DummyClass dummyClass;
+	dummyClass.welcome();
+
+	return 0;
+}
