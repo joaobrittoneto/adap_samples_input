3add61e88dde99bdd053b497aed07718f00ec7ea
Joao Neto <joao.neto@fieb.org.br>
2014-12-03 14:18:58 +0100

    compare and evaluate model with measured data. + levmar lib

--- src/SamplesInput.hpp
+++ src/SamplesInput.hpp
@@ -36,35 +36,33 @@ namespace adap_samples_input
 			//////////////////////////////////////////////////////////////
 			// Position-Velocity Data Processing
 			///////////////////////////////////////////////////////////////
-			// Euler's derivative
-			double Deriv(double sample, double last_sample, double delta_t);
 			// Fitler_SV: Savitzky-Golay filter. queue: RBS. t_th position in queue to be take in account. Converge for a polynomial n_th order. Calculate the smooth position, velocity(&actual_RBS) and acceleration(&actual_RBA)
-			void Filter_SV(std::queue<base::samples::RigidBodyState> &queue, double t, double n, base::samples::RigidBodyState &actual_RBS, base::samples::RigidBodyAcceleration &actual_RBA);
+			void Filter_SV(std::queue<base::samples::RigidBodyState> &queue, double t, double n, double step, base::samples::RigidBodyState &actual_RBS, base::samples::RigidBodyAcceleration &actual_RBA);
 			// Covert: LaserScan->RBS
-			void Convert(base::samples::LaserScan sample, base::samples::RigidBodyState &output);
+			void Convert(base::samples::LaserScan &sample, base::samples::RigidBodyState &output);
 			// Remove a outlier from sample before pushing it into the queue
 			void Remove_Outlier(std::queue<base::samples::RigidBodyState> &queueOfPosition, base::samples::RigidBodyState &sample);
 			// Compute the velocity and the acceleration (based on ()Filter_SV and (X)Euler's method)
-			void Velocity (std::queue<base::samples::RigidBodyState> &queueOfRBS, int size, base::samples::RigidBodyState &actual_RBS, base::samples::RigidBodyState &last_RBS, base::samples::RigidBodyAcceleration &actual_RBA);
+			void Velocity (std::queue<base::samples::RigidBodyState> &queueOfRBS, int size, base::samples::RigidBodyState &actual_RBS, base::samples::RigidBodyAcceleration &actual_RBA);
 			// Method used in the Task, orogen component.
-			void Update_Velocity(base::samples::LaserScan sample_position, std::queue<base::samples::RigidBodyState> &queueOfRBS, int size, base::samples::RigidBodyState &actual_RBS, base::samples::RigidBodyAcceleration &actual_RBA);
+			void Update_Velocity(base::samples::LaserScan &sample_position, std::queue<base::samples::RigidBodyState> &queueOfRBS, int size, base::samples::RigidBodyState &actual_RBS, base::samples::RigidBodyAcceleration &actual_RBA);
 
 			//////////////////////////////////////////////////////////////
 			// Force Data Processing
 			///////////////////////////////////////////////////////////////
 			// Convert from PWM->Newton
-			void ConvertForce(base::samples::Joints sample, base::samples::Joints &forcesTorques);
+			void ConvertForce(base::samples::Joints &sample, base::samples::Joints &forcesTorques);
 			// From PWM->DC. Verify constant used
-			void PWMtoDC(base::Vector6d input, base::Vector6d &output);
+			void PWMtoDC(base::Vector6d &input, base::Vector6d &output);
 			// From DC->N for each thruster. Verify constant used
-			void Forces(base::Vector6d input, base::Vector6d &output);
+			void Forces(base::Vector6d &input, base::Vector6d &output);
 			// From N->N. From each thruster to net forces and torque in the AUV. Verify TCM matrix
-			void ForcesTorques(base::Vector6d input, base::Vector6d &output);
+			void ForcesTorques(base::Vector6d &input, base::Vector6d &output);
 			// Method used in the Task, orogen component.
-			void Update_Force(base::samples::Joints sample, std::queue<base::samples::Joints> &queueOfForces, int size, base::samples::Joints &forces_output);
+			void Update_Force(base::samples::Joints &sample, std::queue<base::samples::Joints> &queueOfForces, int size, base::samples::Joints &forces_output);
 
 			// Compensate the position-filter delays, aligning the forces and velocities
-			void Delay_Compensate(base::samples::RigidBodyState actual_RBS, std::queue<base::samples::Joints> queueOfForces, base::samples::Joints &forces_output);
+			void Delay_Compensate(base::samples::RigidBodyState &actual_RBS, std::queue<base::samples::Joints> &queueOfForces, base::samples::Joints &forces_output);
 
 
 			// Construct a queue
